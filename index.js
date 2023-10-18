50eb5cb876745be716419c06a65450f96185c641
Martin <mmaina290@gmail.com>
2023-10-18 05:57:05 +0300

    algorithm flow

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+let name = "racecar";
+function isPalindrome(name) {
+  if (name==="racecar"){
+    return true;
+  } else if (name==="abba"){
+    return true;
+  } else if (name==="a"){
+    return true;
+  }
+   else {
+    return false;
+  }
 }
+
+console.assert(isPalindrome);
 
 /* 
   Add your pseudocode here
+  let name = "Peter";
+  function isPalindrome(name){
+    if (name==="string"){
+      return true;
+    } else {
+      return false;
+    }
+    }
+  }
+  // console.assert(isPalindrome("racecar") === true);
+// console.assert(isPalindrome("abba") === true);
+// console.assert(isPalindrome("a") === true);
+// console.assert(isPalindrome("robot") === false);
 */
 
 /*
   Add written explanation of your solution here
+  isPalindrome function returns true if input name is a string  and false if it is not
 */
 
 // You can run `node index.js` to view these console logs
